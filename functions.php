6f3d6dcc915ff5b2188215a4d8f832123e73e07a
ptwickler <peter.twickler@gmail.com>
2015-05-19 16:45:31 -0400

    first to create

---
+++ functions.php
@@ -0,0 +1,411 @@
+<?php
+date_default_timezone_set ( 'America/New_York' );
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+
+#----------------------#
+# Product List         #
+#----------------------#
+
+$products = array(
+    'quartzorb' => array(
+        'img' => 'quartzorb',
+        'name' => 'Quartz Orb',
+        'material' => 'quartz',
+        'price' => '30.00',
+        'weight' => '2lbs'
+    ),
+
+    'amethyst'=> array(
+        'img' => 'amethyst',
+        'name' => 'Amethyst',
+        'material' => 'amethyst',
+        'price' => '10.00',
+        'weight'=> '.5lbs'
+    ),
+
+    'catseye'=> array(
+        'img' => 'catseye',
+        'name' => 'Cats Eye',
+        'material' => 'cats eye',
+        'price' => '3.00',
+        'weight' => '.02lbs'
+    ),
+
+    'wizard' => array(
+        'img' => 'wizard',
+        'name' => 'Wizard',
+        'material' => 'pewter and quartz',
+        'price' => '40.00',
+        'weight' => '1lb'
+    ),
+
+    'dragon' => array(
+        'img' => 'dragon',
+        'name' => 'Dragon',
+        'material' => 'pewter and amethyst',
+        'price' => '50.00',
+        'weight' => '3lbs'
+    ),
+
+    'elf' => array(
+        'img'=> 'elf',
+        'name' => 'Elf',
+        'material' => 'pewter',
+        'price' => '20.00',
+        'weight' => '2lbs'
+    )
+);
+
+
+
+function db_connect(){
+    $host = '127.0.0.1';
+    $user = 'twickler';
+    $pw = '123456';
+    $database = 'cart';
+
+    $db = new mysqli($host,$user,$pw,$database) or die("Cannot connect to MySQL.");
+
+    return $db;
+
+}
+
+
+#----------------------#
+# Functions  checkout  #
+#----------------------#
+// finish the out_cart indexing to pull in the items.
+function confirm_email($user,$products) {
+
+    $message = "<html><head></head><body><br><br><br><br><br><br><br>" . $user.", thank you for buying this stuff.<br>Your Purchases:";
+
+    $user_list = file('accounts.txt');
+    for($i=0; $i < count($user_list);$i++) {
+        $line = explode(",",$user_list[$i]);
+        for ($c = 0; $c < count($line); $c++) {
+            $user_match = preg_match('/^' . $user . '$/', $line[$c], $matches);
+
+            if ($matches) {
+                $user_email = $line[1]; //This is the index of the user info that stores the email address.
+                $to = $user_email;
+
+                $email_subject = $user . "-- Your Purchase from Crystals, Charms, and Coffee " . date("F d, Y h:i a");
+
+                //$message = '';
+                $total = 0;
+
+                foreach($_SESSION['out_cart'] as $key=>$value) {
+                    $product = $products[$key];
+
+                    $message .= '<table><tbody><tr><td class="checkout_name">' . $product['name'] . '</td><td class="checkout_quantity">' . $_SESSION['out_cart'][$key]['quantity'] . '</td><td class="checkout_price">$' . $product['price'] * intval($_SESSION['out_cart'][$key]['quantity']) .'.00</td></tr>';
+                    $total += $product['price'] * intval($_SESSION['out_cart'][$key]['quantity']);
+                }
+
+                $message .= '</tbody></table><div class="total_price"> Your Total: $' .$total . '.00</div></body></html>';
+
+                $headers  = "From: peter.twickler@gmail.com" . "\r\n";
+                $headers .= 'MIME-Version: 1.0' . "\n";
+                $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+                $mail = mail($to, $email_subject, $message,$headers);
+
+                // If the order went through and the email worked, display the confirmation message and unset the cart.
+                if ($mail == true) {
+                    $thanks =  "Thank you for your purchase, ". $user . ". An email with your purchase receipt has been sent to your email address.<br><br>
+                    Your friends at Crystals, Charms, and Coffees";
+                    unset($_SESSION['out_cart']);
+
+                }
+
+                elseif($mail != true) {
+                    $thanks = "I'm sorry, something went wrong and we could not send your receipt to the email address on file.";
+                }
+
+            }
+        }
+
+    }
+    return $thanks;
+}
+
+
+/*
+ * Builds the string to display the products and info in the user's cart.
+ */
+function build_out_cart($cart = NULL, $products){
+
+    $out_cart = '';
+    $total = 0;
+
+    if ($cart) {
+        foreach ($cart as $key => $value) {
+            $product = $products[$key];
+
+            $out_cart .= '<tr><td class="checkout_name">' . $product['name'] . '</td><td class="checkout_quantity">' . $cart[$key]['quantity'] . '</td><td class="checkout_price">$' . $product['price'] * intval($cart[$key]['quantity']) . '.00</td></tr>';
+            $total += $product['price'] * intval($cart[$key]['quantity']);
+        }
+
+        $out_cart .= '</tbody></table><div class="total_price"> Your Total: $' . $total . '.00</div>';
+        return $out_cart;
+    }
+}
+
+#----------------------#
+# Functions  index     #
+#----------------------#
+
+/*
+ * Pulls the "properties" of the product arrays into a string to build the html for the display of the products
+ *
+ *  $item is the product being processed and $products is the array of products in products.php.s
+ */
+function display($item,$products){
+    $db = $db_connect();
+// Pushes the properties of the items into the html to display them. I use a hidden, read-only input called "item"
+    // in the form to get the "machine" name of the item for use as an index later. I couldn't figure out a better way
+    // to do this.
+    $product_display =  '<form  method="GET" action="functions.php?add_cart=1">
+                         <div class = "product_display">
+                         <input class="disp_name" type="text" value = "'.$products[$item]["name"] .'" name="prod_name" readonly>
+                         <div class ="prod_img" ><img src = "./img/' . $products[$item]["img"].'.jpg"></div>
+                         <div class = "prod_weight">'.$products[$item]["weight"] . '</div>
+                         <div class = "prod_price">$'.$products[$item]["price"].'</div>
+
+                         <input type="text" size="5" name="quantity">
+                         <input class="add_to_cart"  type="submit" value="Add to Cart" >
+                         <input type="text" name ="item" value="'.$item.'" readonly hidden="true">
+                         </form>
+
+    </div>';
+
+    return $product_display;
+
+};
+
+// Grabs the items out of the cart and gets their relevant details from the array in products.php which it then pushes
+// into the "out cart" which will be used to create the shopping cart page.
+
+function add_to_cart($products,$item,$quantity){
+
+    $item = $item;
+    $products = $products;
+    $_SESSION['out_cart'][$item]['name'] = $item;
+    $_SESSION['out_cart'][$item]['quantity'] = $quantity;
+    $url = "http://" . $_SERVER['HTTP_HOST'] . "/sql_final/index.php";
+    header("Location: " . $url) or die("Didn't work");
+}
+
+// This bit calls the above function. I wanted to put it on the index.php page, so it would be cleaner, but I couldn't
+// figure it out in time. So, you get the below kludge.
+if (isset($_GET['prod_name']) && $_GET['prod_name'] != 1) {
+
+    $item = $_GET['item'];
+    $quantity = $_GET['quantity'];
+    $cart = $_SESSION['cart'];
+
+    if ($_SESSION['sign_in'] == 1) {
+        add_to_cart($products, $item, $quantity);
+        $url = "http://" . $_SERVER['HTTP_HOST'] . "/sql_final/index.php";
+        header("Location: " . $url) or die("Didn't work");
+    } else {
+
+        $url = "http://" . $_SERVER['HTTP_HOST'] . "/sql_final/index.php?signed=0";
+        header("Location: " . $url) or die("Didn't work");
+    }
+}
+
+#----------------------#
+# Functions login      #
+#----------------------#
+
+// This function inserts a new "account" into the accounts.txt file. This is how I keep track of login credentials.
+// Basically, it implodes the values into a string and then writes it to the file accounts.txt.
+function new_user($user,$pass,$email) {
+
+    $n_user = $user;
+
+    $n_pass = $pass;
+    $n_email = $email;
+
+    $users_list = fopen('/Library/WebServer/Documents/sql_final/accounts.txt','a+');
+
+    $user_values = array($n_user,$n_pass,$n_email);
+
+    $user_in = implode(",",$user_values);
+
+    $user_in_line = PHP_EOL . $user_in;
+
+    fwrite($users_list,$user_in_line);
+
+    fclose($users_list);
+}
+
+// Builds the new user registration form. Different states are for form validation. If any of the session variable
+// "valid" properties are set,it displays the correct error message.
+function register_display($session) {
+
+    if (isset($session['valid']['name']) && $session['valid']['name'] == 'name_error' ) {
+
+        $register_display =  '<form name="register" action="index.php?new_user=1" method="POST">
+             <input type="text" size="20" name="username">
+              <label for="username"><span class="form_error">Please enter a valid username.</span></label><br />
+             <input type="text" size="20" name="email">
+             <label for="email">Enter Your email address</label><br />
+             <input type="text" size="20" name="password">
+             <label for="password">Enter your password</label><br/>
+             <input type="submit" value="Click to register!">
+           </form>';
+    }
+
+    elseif(isset($session['valid']['email']) && $session['valid']['email'] == 'email_error' ) {
+        $register_display =  '<form name="register" action="index.php?new_user=1" method="POST">
+             <input type="text" size="20" name="username">
+              <label for="username">Enter your name</label><br />
+             <input type="text" size="20" name="email">
+             <label for="email"><span class="form_error">Please enter a valid email address.</span></label><br />
+             <input type="text" size="20" name="password">
+             <label for="password">Enter your password</label><br/>
+             <input type="submit" value="Click to register!">
+           </form>';
+
+    }
+
+    elseif(isset($session['valid']['password']) && $session['valid']['password'] == 'password_error' ) {
+        $register_display =  '<form name="register" action="index.php?new_user=1" method="POST">
+             <input type="text" size="20" name="username">
+              <label for="username">Enter your name</label><br />
+             <input type="text" size="20" name="email">
+             <label for="email">Enter your email address</label><br />
+             <input type="text" size="20" name="password">
+             <label for="password"><span class="form_error">Please enter a valid password.</span></label><br/>
+             <input type="submit" value="Click to register!">
+           </form>';
+    }
+
+    else {
+        $register_display = '<form name="register" action="index.php?new_user=1" method="POST">
+             <input type="text" size="20" name="username">
+              <label for="name">Enter your name</label><br />
+             <input type="text" size="20" name="email">
+             <label for="email">Enter your email address</label><br />
+             <input type="text" size="20" name="password">
+             <label for="password">Enter a password</label><br />
+             <input type="submit" value="Click to register!">
+           </form>';
+    }
+    return $register_display;
+}
+
+/*
+ * @param Takes $_POST['username'] and $_POST['password']
+ */
+// If the user has submitted the login form, iterate through the records in accounts.txt.
+// The nested for loops iterate first through the file, line by line, and then the first nested for loop
+// iterates through the line looking first for the username submitted. If it finds the username,
+// it then iterates through the same line again looking for the password. If both are found, the user is
+// logged in. If the password isn't found, it suggests you try again. If the user isn't found, it displays
+// the registration form.
+function user_cred($query=array()) {
+    $user_info = $query;
+
+    // Form validation and processing. If the new_user variable is set, test the form inputs and then process.
+    if(isset($_GET['new_user']) && $_GET['new_user'] ==1){
+        $name_test = $user_info['username'];
+
+        if ($name_test != null && $name_test != '') {
+            $user_name = $name_test;
+        }
+
+        // I have set $user_info to the query (POST) and so now I pass that along instead of the $_POST. I hope to
+        // avoid confusion by doing so. Whether or not the $_POST['email'] property is set is the test for distinguishing
+        // the regular non-validation path from the form validation path.
+        elseif (($name_test == '' || $name_test == null) && isset($_POST['email'])) {
+            $_SESSION['valid']['name'] = 'name_error';
+            $url = "http://" . $_SERVER['HTTP_HOST'] . "/sql_final/index.php?register_new=1";
+            header("Location: " . $url) or die("didn't redirect from login");
+        }
+
+        $user_email = $_POST['email'];
+        if ($user_email && $user_email != null) {
+            $email_check = filter_var($user_email, FILTER_VALIDATE_EMAIL);
+        }
+
+        if($user_email == null || $email_check != true){
+            $_SESSION['valid']['email'] = 'email_error';
+
+            $url = "http://" . $_SERVER['HTTP_HOST'] . "/sql_final/index.php?register_new=1";
+            header("Location: " . $url) or die("didn't redirect from login");
+
+        }
+
+        $user_pw = $user_info['password'];
+        if ($user_pw == null or !isset($user_pw)){
+            $_SESSION['valid']['password'] = 'password_error';
+
+            $url = "http://" . $_SERVER['HTTP_HOST'] . "/sql_final/index.php?register_new=1";
+            header("Location: " . $url) or die("didn't redirect from login");
+        }
+
+        new_user($user_name,$user_email,$user_pw);
+
+        ob_clean();
+        $url = "http://" . $_SERVER['HTTP_HOST'] . "/sql_final/index.php";
+        header("Location: " . $url) or die("didn't redirect from login");
+    }
+
+    $username = $_POST['username'];
+
+    $pw = $_POST['password'];
+
+    $reg_link = 0; // Counter to limit the display of the "register here" verbiage.
+    $pass_error = 0;
+
+    $user_list = file('accounts.txt');
+
+    // Iterates through the file testing each line for the username and password combo.
+    for ($i = 0; $i < count($user_list); $i++){
+        $line = explode(",",$user_list[$i]);
+
+        for ($c = 0; $c < count($line); $c++) {
+            $user_match =  preg_match('/^' . $username . '$/', $line[$c], $matches);
+
+            if ($matches) {
+
+                for ($p = 0; $p < count($line); $p++) {
+
+                    for ($p = 0; $p < count($line); $p++) {
+
+                        $pw_match = preg_match('/^' . $pw . '$/', $line[$p], $match);
+
+                    }
+
+                    if ($pw_match) {
+                        $_SESSION['sign_in'] = 1;
+                        $_SESSION['username'] = $username;
+                        $url = "http://" . $_SERVER['HTTP_HOST'] . "/sql_final/index.php";
+                        ob_clean();
+                        header("Location: " . $url) or die("didn't redirect from login");
+
+                    }
+
+                    elseif ($matches && $pw_match == false) {
+                        if($pass_error == 1)
+                            echo '<span class="form_error">The password you entered is not correct</span>';
+                        $pass_error++;
+                    }
+                }
+            }
+
+            elseif (!$matches) {
+                if ($reg_link == 1) break;
+                echo '<div>Not registered? Click <a href="index.php?register_new=1">here</a> to register.</div>';
+                $reg_link++; // Increments counter to control the number of times the above verbiage and link are displayed.
+
+            }
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+ * The index page controls the user's path through the site. There are three different "modes" which will cause the
+ * page to display differently. First, the "normal" mode which is a user is either signed in, or not, but has not
+ * selected that they want to check out (products are displayed). Second, "checkout" mode. The user must be signed in to
+ * click the checkout button. If selected, the products are not displayed, but the user's items for purchase are.
+ * Thirdly, "register_new" mode. It displays the products, but also displays the registration form.
+ */
+
+ob_start();
+
+
+require_once('functions.php');
+
+if (isset($_GET['out']) && $_GET['out']==1){
+    session_start();
+
+    // I'll be honest. I had to poach the code in this if statement. I couldn't get the session ID to regenerate any
+    // other way. It would wipe out the data in the session, but it wouldn't renew the session ID. I was having some
+    // other problems and wanted to get this piece nailed down before I went on to look for another cause.
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+// END POACHED CODE
+
+    // Finally, destroy the session.
+    session_destroy();
+
+    //Add in a page reload so that the session_destroy() will take effect
+
+    $url = "http://" . $_SERVER['HTTP_HOST'] . "/sql_final/index.php";
+
+    header("Location: ".$url) or die("Didn't work");
+}
+
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+$_SESSION['cart'] = array();
+
+// The valid property of the session will be used to store values for form validation. If it's set, don't change it,
+// but if it's not set, set it.
+if (!isset($_SESSION['valid'])) {
+    $_SESSION['valid'] = array();
+}
+
+include_once($_SERVER['DOCUMENT_ROOT'] ."/sql_final/template_top.inc");
+
+
+// This array stores the machine names of the products. It needs to be appended if you want to add another
+// product to the store. The products.php file must also be appended to contain the new item's properties.
+$current_products = array('amethyst','quartzorb','wizard','catseye','dragon');
+
+// This if statement tests for the username and passwords in the POST variable. If they are there, it activates the
+// login.
+if (isset($_POST['username']) && isset($_POST['password'])) {
+
+    user_cred($_POST);
+
+}
+
+// Activates the register form if a user has submitted an username not in the accounts.txt list. As of now, blank
+// submissions of the login form also trigger the register form display.
+if (isset($_GET['register_new']) && $_GET['register_new'] == 1) {
+
+    $register_display = register_display($_SESSION);
+
+    echo $register_display;
+}
+
+// This puts the site into "checkout mode".
+if (isset($_GET['checkout']) && $_GET['checkout'] ==1 ){
+    $items = $_SESSION['out_cart'];
+    $out_table = build_out_cart($items,$products);
+
+    echo '
+<div class="cart_display">
+          <h2>Your Cart:</h2>
+          <hr>
+          <br>
+          <br>
+            <table><tbody><th>Item</th><th>Quantity</th><th>Price</th>' . $out_table .  '</div><br><br><br><br>
+<form name = "purchase" action="index.php?checkout=1" method="POST">
+  <input type="text" hidden name="mail" value="1">
+  <input type="submit" value="complete purchase">
+</form>
+</div>
+<div>
+  <a href="index.php">Continue Shopping!</a>
+
+</div>';
+
+    // If the post variable "mail" is set and equals 1, send the confirmation email and display the confirmation message.
+    if (isset($_POST['mail']) && $_POST['mail'] == 1) {
+        $thanks = confirm_email($_SESSION['username'],$products);
+
+
+        if ($thanks){
+            echo $thanks;
+        }
+
+        if (!$thanks) {
+            echo "There was a problem and we could not send your confirmation email";
+        }
+
+    }
+    echo '</body></html>';
+}
+
+// If none of the other "special case" query strings are set, the script displays the products. That is, the site
+// is in "shopping mode".
+else {
+    $disp = '';
+
+    for ($i = 0; $i < count($current_products); $i++) {
+        echo display($current_products[$i], $products);
+    }
+
+
+    echo "</div><!--end div.wrapper-->";
+
+    echo '</body>
+</html>';
+}
+
+
+
+
